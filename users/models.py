03843297edafeb68b7859aa1365b6f122457bf7e
visl <5vladik068@gmail.com>
2022-10-21 16:48:43 +0300

    Signals

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
-
 # Create your models here.
 
 class Profile(models.Model):
@@ -22,7 +21,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 class Skill(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
