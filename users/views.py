3a2b6f5992ae5c0c49650ec7d41f3f3c750f5119
visl <5vladik068@gmail.com>
2022-10-30 19:19:59 +0200

    User Registration

--- users/views.py
+++ users/views.py
@@ -1,14 +1,16 @@
+from atexit import register
 from multiprocessing import context
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .forms import CustomUserCreationForm
 from .models import Profile
 # Create your views here.
 
 def loginUser(request):
-
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('profiles')
 
@@ -35,6 +37,28 @@ def logoutUser(request):
     logout(request)
     messages.warning(request, "User was succesfully logout")
     return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            #form.save()
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, "User account was created")
+
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.warning(request, "An error occured creating a user")
+
+    context ={'page': page, 'form': form }
+    return render(request, 'users/login_register.html', context)
 
 def profiles(request):
     profiles = Profile.objects.all()
