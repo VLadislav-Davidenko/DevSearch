d913fbc787d5022b7017a73a1778c1a24fc12089
visl <5vladik068@gmail.com>
2022-10-31 22:32:29 +0200

    Fixing issue with not working "x" button in alert messages

--- static/uikit/app.js
+++ static/uikit/app.js
@@ -8,7 +8,8 @@ let alertWrapper = document.querySelector('.alert')
 let alertClose = document.querySelector('.alert__close')
 
 if (alertWrapper) {
-  alertClose.addEventListener('click', () =>
-    alertWrapper.style.display = 'none'
+  alertClose.addEventListener('click', function() {
+     alertWrapper.style.display = 'none'
+  }
   )
 }
