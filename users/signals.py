ff9d02e279211d7457f1cf96c13ecefd26a47493
visl <5vladik068@gmail.com>
2022-11-14 22:59:42 +0200

    User Edit Profile

--- users/signals.py
+++ users/signals.py
@@ -13,6 +13,18 @@ def createProfile(sender, instance, created, **kwargs):
             email = user.email,
             name = user.first_name,
         )
+
+@receiver(post_save, sender=Profile)
+def updateUser(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+    if created == False:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+
+
 @receiver(post_delete, sender=Profile)
 def deleteUser(sender, instance, **kwargs):
     user = instance.user
