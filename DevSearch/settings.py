293cff54dd2a5d06f4c73185f70b1f5ae3b5fd13
Vilsivul <92055005+VLadislav-Davidenko@users.noreply.github.com>
2022-11-25 18:38:01 +0200

    \

--- DevSearch/settings.py
+++ DevSearch/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_za*lc@why^4uj#!&1@yf&7$y6!a+3k5-q(i6+1pff&tk!pz^#'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
