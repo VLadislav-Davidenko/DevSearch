6b18523640c3f0ff7efda0837ff981cdb8126248
Vilsivul <92055005+VLadislav-Davidenko@users.noreply.github.com>
2022-12-05 18:04:35 +0200

    Adding the ability to send messages to other developers

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm, MessageForm
 from .models import Profile, Message
 from .utils import searchProfiles, paginateProfiles
 
@@ -173,3 +173,31 @@ def viewMessage(request, pk):
 
     context = {'singleMessage': singleMessage}
     return render(request, 'users/message.html', context)
+
+
+def createMessage(request, pk):
+    recipient = Profile.objects.get(id=pk)
+    form = MessageForm()
+
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+    
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid:
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = recipient
+
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+            message.save()
+
+        messages.success(request, "Your message was delivered")
+        return redirect('user-profile', pk=recipient.id)
+    
+    context = {'recipient':recipient, 'form': form}
+    return render(request, 'users/message_form.html', context)
