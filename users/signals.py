d8e8f09736983c546e3722e9e4aaa8d811752507
Vilsivul <92055005+VLadislav-Davidenko@users.noreply.github.com>
2022-12-09 17:48:05 +0200

    Welcome message to mail implementation

--- users/signals.py
+++ users/signals.py
@@ -2,6 +2,9 @@ from .models import Profile
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.core.mail import send_mail
+
+from django.conf import settings
 
 @receiver(post_save, sender=User)
 def createProfile(sender, instance, created, **kwargs):
@@ -12,7 +15,20 @@ def createProfile(sender, instance, created, **kwargs):
             username = user.username,
             email = user.email,
             name = user.first_name,
+
         )
+        
+        subject = "Welcome to DevSearch"
+        message = "We are glad you are here with us now!"
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
+
 
 @receiver(post_save, sender=Profile)
 def updateUser(sender, instance, created, **kwargs):
