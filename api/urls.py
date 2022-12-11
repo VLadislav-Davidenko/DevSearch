701a6a5c2edef2661cdb3decc5e08d429237bb4e
Vilsivul <92055005+VLadislav-Davidenko@users.noreply.github.com>
2022-12-11 21:24:37 +0200

    Django REST Framework & Serializers

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,8 @@ from . import views
 # python3 manage.py runserver
 
 urlpatterns = [
-    path('', views.getRoutes, name='projects'),
+    path('', views.getRoutes),
+    path('projects/', views.getProjects),
+    path('projects/<str:pk>/', views.getProject),
 ]
 
