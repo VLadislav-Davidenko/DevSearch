08dbf266ec6c942ab1626cc14f199a3eaa9361d9
Vilsivul <92055005+VLadislav-Davidenko@users.noreply.github.com>
2023-01-10 18:26:56 +0200

    Connecting Live DB AWS Postgres

--- DevSearch/settings.py
+++ DevSearch/settings.py
@@ -128,10 +128,21 @@ WSGI_APPLICATION = 'DevSearch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'devsearch',
+        'USER': 'visl',
+        'PASSWORD': '',
+        'HOST': 'devsearch.cdndrzf6vxdb.us-east-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
